2c90b5b11988beaae301cf8dee256423c67b1793
minieemaia <127688391+minieemaia@users.noreply.github.com>
2023-07-02 16:34:25 -0300

    Create l4q2.cpp

---
+++ l4/l4q2.cpp
@@ -0,0 +1,15 @@
+// Questão 02
+// Aluna: Jasmin Marques 
+// Truma: IF23
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main() {
+  char a[20];
+  cin.get(a,20);
+  for (int i =0; i <4; i++) {
+    cout << a[i];
+  }
+  return 0;
+}
